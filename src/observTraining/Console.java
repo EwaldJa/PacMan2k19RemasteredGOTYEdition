256c9670aed077b2ff476362998d112963f77d22
Ewald <45970173+EwaldJa@users.noreply.github.com>
2019-02-12 17:27:45 +0100

    + Training repositery for Observer and Observable using

---
+++ src/observTraining/Console.java
@@ -0,0 +1,27 @@
+package observTraining;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Console implements Observer {
+
+    private Jeu mygame;
+
+    public Console(Jeu jeu) {
+        mygame = jeu;
+    }
+
+    @Override
+    public void update(Observable arg0, Object arg1) {
+        System.out.println("Grille : ");
+        boolean[][] tab = mygame.getState();
+        for(int i = 0; i < tab.length; i++) {
+            for(int j = 0; j < tab[i].length; j++) {
+                System.out.print(tab[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+}
